e81186e9be22997c227c6e935f727fc45698e15f
Samuel Silva <samuelsilvawb@gmail.com>
2021-12-26 13:46:52 +0000

    feat(app): create reusable button component
    - added button capabilities
    - added link capabilities from react router dom

---
+++ app/src/components/button/Button.tsx
@@ -0,0 +1,45 @@
+import { Link, LinkProps } from "react-router-dom";
+import styles from "./Button.module.css";
+
+interface Props {
+  /**
+   * @default "button"
+   */
+  as?: "button" | "link";
+  to?: LinkProps["to"];
+  children?: React.ReactNode | undefined;
+  className?: string;
+  type?: React.ButtonHTMLAttributes<HTMLButtonElement>["type"];
+  "aria-label"?: string;
+}
+
+function Button({
+  className = "",
+  children,
+  as = "button",
+  type,
+  to,
+  "aria-label": ariaLabel,
+}: Props) {
+  const combinedClassName = `${styles.button} ${className}`;
+
+  if (as === "link") {
+    if (!to) {
+      return null;
+    }
+
+    return (
+      <Link className={combinedClassName} to={to} aria-label={ariaLabel}>
+        {children}
+      </Link>
+    );
+  }
+
+  return (
+    <button className={combinedClassName} type={type} aria-label={ariaLabel}>
+      {children}
+    </button>
+  );
+}
+
+export default Button;
